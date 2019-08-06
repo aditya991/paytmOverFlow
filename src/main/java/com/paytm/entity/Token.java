10dece1558fd02a2914ad637a6dde02f3a7b0f92
aditya991 <aditya10.kumar@paytm.com>
2019-08-06 16:52:17 +0530

    updated code

---
+++ src/main/java/com/paytm/entity/Token.java
@@ -0,0 +1,10 @@
+package com.paytm.entity;
+/*
+ * @author: aditya10.kumar
+ * @created: 06/08/19
+ */
+
+public class Token extends AbstractEntity {
+
+
+}
--- src/main/java/com/paytm/entity/User.java
+++ src/main/java/com/paytm/entity/User.java
@@ -1,8 +1,60 @@
 package com.paytm.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 /*
  * @author: aditya10.kumar
  * @created: 06/08/19
  */
+@Entity
+public class User extends AbstractEntity {
+    @Id
+    private Integer u_id;
+    private String u_name;
+    private String password;
+    @Column(unique = true)
+    private String email;
+    private String phone;
 
-public class User {
+    public Integer getU_id() {
+        return u_id;
+    }
+
+    public void setU_id(Integer u_id) {
+        this.u_id = u_id;
+    }
+
+    public String getU_name() {
+        return u_name;
+    }
+
+    public void setU_name(String u_name) {
+        this.u_name = u_name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
