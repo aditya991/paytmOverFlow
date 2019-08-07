242b6bfd04c80fe44aedb90b3500c45ad9ec87ab
aditya991 <aditya10.kumar@paytm.com>
2019-08-07 10:27:40 +0530

    updated code 7th August

--- src/main/java/com/paytm/entity/Token.java
+++ src/main/java/com/paytm/entity/Token.java
@@ -4,7 +4,44 @@ package com.paytm.entity;
  * @created: 06/08/19
  */
 
-public class Token extends AbstractEntity {
+import java.util.Date;
 
+public class Token extends AbstractEntity
+{
+    private Integer token_id;
+    private String token_no;
+    private Date expiry_time;
+    private Integer flag;
 
+    public Integer getToken_id() {
+        return token_id;
+    }
+
+    public void setToken_id(Integer token_id) {
+        this.token_id = token_id;
+    }
+
+    public String getToken_no() {
+        return token_no;
+    }
+
+    public void setToken_no(String token_no) {
+        this.token_no = token_no;
+    }
+
+    public Date getExpiry_time() {
+        return expiry_time;
+    }
+
+    public void setExpiry_time(Date expiry_time) {
+        this.expiry_time = expiry_time;
+    }
+
+    public Integer getFlag() {
+        return flag;
+    }
+
+    public void setFlag(Integer flag) {
+        this.flag = flag;
+    }
 }
